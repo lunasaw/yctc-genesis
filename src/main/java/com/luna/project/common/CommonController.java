68b794659105b89ff7bc80f9f07c765a6162141a
luna <iszychen@gmail.com>
2020-03-28 17:00:44 +0800

    文件上传下载

--- src/main/java/com/luna/project/common/CommonController.java
+++ src/main/java/com/luna/project/common/CommonController.java
@@ -2,11 +2,14 @@ package com.luna.project.common;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -94,7 +97,7 @@ public class CommonController
     /**
      * 本地资源通用下载
      */
-    @GetMapping("/common/download/resource")
+    @GetMapping("/common/download/resource/")
     public void resourceDownload(String resource, HttpServletRequest request, HttpServletResponse response)
             throws Exception
     {
