bcccc24c2f076a9f5e845a55596dc3b7aa770b9a
luna <iszychen@gmail.com>
2020-04-16 20:58:50 +0800

    course 课程信息

--- src/main/java/edu/yctc/project/system/classes/domain/Classes.java
+++ src/main/java/edu/yctc/project/system/classes/domain/Classes.java
@@ -2,6 +2,7 @@ package edu.yctc.project.system.classes.domain;
 
 import edu.yctc.framework.aspectj.lang.annotation.Excel;
 import edu.yctc.framework.web.domain.BaseEntity;
+import edu.yctc.project.system.academy.domain.Academy;
 import edu.yctc.project.system.dept.domain.Dept;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -12,75 +13,79 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author yctc
  * @date 2020-04-16
  */
-public class Classes extends BaseEntity
-{
+public class Classes extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
     /** 编号 */
-    private Long id;
+    private Long              id;
 
     /** 学院编号 */
     @Excel(name = "学院编号")
-    private Long academyId;
+    private Long              academyId;
 
     /** 专业编号 */
     @Excel(name = "专业编号")
-    private Long deptId;
+    private Long              deptId;
 
     /** 班级编号 */
     @Excel(name = "班级编号")
-    private String number;
+    private String            number;
 
-	private Dept dept;
+    private Academy           academy;
 
-	public Dept getDept()
-	{
-		if (dept == null)
-		{
-			dept = new Dept();
-		}
-		return dept;
-	}
+    private Dept              dept;
 
-	public void setDept(Dept dept)
-	{
-		this.dept = dept;
-	}
+    public Academy getAcademy() {
+        if (academy == null) {
+            academy = new Academy();
+        }
+        return academy;
+    }
 
-    public void setId(Long id) 
-    {
+    public void setacademy(Academy academy) {
+        this.academy = academy;
+    }
+
+    public Dept getDept() {
+        if (dept == null) {
+            dept = new Dept();
+        }
+        return dept;
+    }
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() 
-    {
+    public Long getId() {
         return id;
     }
-    public void setAcademyId(Long academyId) 
-    {
+
+    public void setAcademyId(Long academyId) {
         this.academyId = academyId;
     }
 
-    public Long getAcademyId() 
-    {
+    public Long getAcademyId() {
         return academyId;
     }
-    public void setDeptId(Long deptId) 
-    {
+
+    public void setDeptId(Long deptId) {
         this.deptId = deptId;
     }
 
-    public Long getDeptId() 
-    {
+    public Long getDeptId() {
         return deptId;
     }
-    public void setNumber(String number) 
-    {
+
+    public void setNumber(String number) {
         this.number = number;
     }
 
-    public String getNumber() 
-    {
+    public String getNumber() {
         return number;
     }
 
---
+++ src/main/java/edu/yctc/project/system/course/service/ICourseService.java
@@ -0,0 +1,61 @@
+package edu.yctc.project.system.course.service;
+
+import java.util.List;
+import edu.yctc.project.system.course.domain.Course;
+
+/**
+ * 课程信息Service接口
+ * 
+ * @author yctc
+ * @date 2020-04-16
+ */
+public interface ICourseService 
+{
+    /**
+     * 查询课程信息
+     * 
+     * @param id 课程信息ID
+     * @return 课程信息
+     */
+    public Course selectCourseById(Long id);
+
+    /**
+     * 查询课程信息列表
+     * 
+     * @param course 课程信息
+     * @return 课程信息集合
+     */
+    public List<Course> selectCourseList(Course course);
+
+    /**
+     * 新增课程信息
+     * 
+     * @param course 课程信息
+     * @return 结果
+     */
+    public int insertCourse(Course course);
+
+    /**
+     * 修改课程信息
+     * 
+     * @param course 课程信息
+     * @return 结果
+     */
+    public int updateCourse(Course course);
+
+    /**
+     * 批量删除课程信息
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteCourseByIds(String ids);
+
+    /**
+     * 删除课程信息信息
+     * 
+     * @param id 课程信息ID
+     * @return 结果
+     */
+    public int deleteCourseById(Long id);
+}
